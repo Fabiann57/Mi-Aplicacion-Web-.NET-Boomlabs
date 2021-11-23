6e69028733d5bae50fa9488eca761276fdf0a8a3
Fabian Niño <guitarxkrash@gmail.com>
2021-11-22 19:42:07 -0500

    Primer commit

---
+++ Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.Models
+{
+    public class Usuario
+    {
+        public decimal Id { get; set; }
+        public string Email { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
